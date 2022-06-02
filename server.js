804a1537434904a963f5e669ac1cd6a323439ecc
Victor Rocha <36079471+hugoxy@users.noreply.github.com>
2022-06-02 14:37:26 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import app from './src/app.js'
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost:${port}`);
-})
+app.listen(process.env.PORT || 3000);
+
+//app.listen(port, () => {
+//    console.log(`Servidor escutando em http://localhost:${port}`);
+//})
